bbb3304d2d38e1ed6e4be91bbdb30ad86f8c2e16
Agasyan <agasyan222@gmail.com>
2025-04-24 15:38:37 +0400

    create main file

---
+++ api/main.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        "main:app",
+        reload=True,
+        host="localhost",
+        port=8000,
+    )

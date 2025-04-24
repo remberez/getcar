0b9569d99ff99a9dc90719a1b2a917e1f13eb2eb
Agasyan <agasyan222@gmail.com>
2025-04-24 15:41:11 +0400

    create config.py

---
+++ api/config.py
@@ -0,0 +1,8 @@
+class ProjectSettings:
+    database_url: str = "postgresql+asyncpg://user:password@127.0.0.1:5433/getcar"
+    server_reload: bool = True
+    server_host: str = "localhost"
+    server_port: str = 8000
+
+
+settings = ProjectSettings()
--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
+from config import settings
 
 app = FastAPI()
 
@@ -8,7 +9,7 @@ app = FastAPI()
 if __name__ == '__main__':
     uvicorn.run(
         "main:app",
-        reload=True,
-        host="localhost",
-        port=8000,
+        reload=settings.server_reload,
+        host=settings.server_host,
+        port=settings.server_port,
     )

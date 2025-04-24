e419e8adb2c5a2c12d1db6ce64db8de9dcc2f5ef
Agasyan <agasyan222@gmail.com>
2025-04-24 16:30:33 +0400

    create user model

---
+++ api/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from decimal import Decimal
+from enum import Enum
+
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy import DateTime, Enum as SQLEnum, Numeric, ForeignKey, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class UserRoles(Enum):
+    ADMIN = "admin"
+    USER = "user"
+
+
+class Base(DeclarativeBase):
+    ...
+
+
+class IntegerIDMixin:
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
+class UserModel(Base, IntegerIDMixin, SQLAlchemyBaseUserTable[int]):
+    phone: Mapped[str]
+    full_name: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    role: Mapped[UserRoles] = mapped_column(SQLEnum(UserRoles))
+    balance: Mapped[Decimal] = mapped_column(Numeric(12, 2))

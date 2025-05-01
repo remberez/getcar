c49c7d9468b303e6125f683fc7f89f3f39fe3961
Agasyan <agasyan222@gmail.com>
2025-05-01 19:40:33 +0400

    create car card list component

---
+++ app/src/components/CarCardsList.jsx
@@ -0,0 +1,17 @@
+import CarCard from "./CarCard,";
+
+const CarCardList = ({ carList }) => {
+    return (
+        <section className="flex flex-col gap-y-10 mt-12 container">
+            {
+                carList.map(value => (
+                    <div key={value.id}>
+                        <CarCard car={value}/>
+                    </div>
+                ))
+            }
+        </section>
+    )
+}
+
+export default CarCardList;
--- app/src/pages/SearchPage.jsx
+++ app/src/pages/SearchPage.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+import CarCardList from "../components/CarCardsList";
 import CarCategoryFilter from "../components/CarCategoryFilter";
+import carService from "../serives/cars";
 
 const SearchPage = () => {
+    const [carList, setCarList] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await carService.getAllCars({});
+            setCarList(data);
+        }
+
+        fetchData();
+    }, [])
     return (
         <>
             <CarCategoryFilter/>
+            <CarCardList carList={carList}/>
         </>
     )
 }

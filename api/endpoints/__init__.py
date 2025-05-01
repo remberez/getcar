b549d2dd63c87ac2432506a9e34f2ef9c59c71b1
Agasyan <agasyan222@gmail.com>
2025-05-01 16:47:52 +0400

    create car brand crud router

--- api/endpoints/__init__.py
+++ api/endpoints/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from .auth import router as auth_router
 from .users import router as user_router
 from .rental_class import router as rental_router
+from .car_brand import router as car_brand_router
 
 router = APIRouter(prefix="/api")
 
 router.include_router(router=auth_router)
 router.include_router(router=user_router)
 router.include_router(router=rental_router)
+router.include_router(router=car_brand_router)

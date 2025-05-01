f0e457636d052831aa117f742e721f073232abab
Agasyan <agasyan222@gmail.com>
2025-05-01 14:09:10 +0400

    create header component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,9 @@
+import { Header } from "./components/Header"
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Header/>
     </>
   )
 }

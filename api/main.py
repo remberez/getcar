ca72dd2e9272104d5bc14cb7080d16adadb8c727
Agasyan <agasyan222@gmail.com>
2025-05-01 16:35:03 +0400

    add cors settings

--- api/main.py
+++ api/main.py
@@ -1,11 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from config import settings
 from endpoints import router
 
 app = FastAPI()
 app.include_router(router=router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://localhost:5173",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == '__main__':
